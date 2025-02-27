2635f7a614bfb67a081877f70151d629b3789a5d
ajeetraj11 <ajitpandey110920@gmail.com>
2025-02-27 16:38:25 +0530

    [Ajeet Raj] Add. Implement in-memory storage for Employee Payroll data in Service Layer

--- src/main/java/com/bridgelabz/employeepayrollapp/controller/EmployeeController.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package com.bridgelabz.employeepayrollapp.controller;
 
 import com.bridgelabz.employeepayrollapp.dto.EmployeeRequestDTO;
 import com.bridgelabz.employeepayrollapp.dto.EmployeeResponseDTO;
+import com.bridgelabz.employeepayrollapp.model.Employee;
 import com.bridgelabz.employeepayrollapp.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -15,26 +16,32 @@ public class EmployeeController {
     private EmployeeService employeeService;
 
     // GET Employee
-    @GetMapping("/get")
-    public EmployeeResponseDTO getEmployee() {
-        return employeeService.getEmployee();
+    @GetMapping("/getById/{id}")
+    public EmployeeResponseDTO getEmployee(@PathVariable Long id) {
+        try {
+            Employee employee = employeeService.getEmployeeById(id);
+            return new EmployeeResponseDTO(employee.getName() , employee.getSalary());
+        }catch (Exception e){
+            e.getStackTrace();
+        }
+      return null;
     }
 
     // POST - Add Employee
     @PostMapping("/create")
-    public EmployeeResponseDTO addEmployee(@RequestBody EmployeeRequestDTO employeeRequestDTO) {
-        return employeeService.addEmployee(employeeRequestDTO.getName(), employeeRequestDTO.getSalary());
+    public Employee addEmployee(@RequestBody EmployeeRequestDTO employeeRequestDTO) {
+        return employeeService.addEmployee( new Employee(employeeRequestDTO.getId() ,employeeRequestDTO.getName(), employeeRequestDTO.getSalary()));
     }
 
     // PUT - Update Employee
     @PutMapping("/update/{id}")
-    public String updateEmployee(@PathVariable Long id) {
-        return employeeService.updateEmployee(id);
+    public Employee updateEmployee(@PathVariable Long id , @RequestBody EmployeeRequestDTO employeeRequestDTO) {
+        return employeeService.updateEmployee(id , new Employee(employeeRequestDTO.getId() , employeeRequestDTO.getName() , employeeRequestDTO.getSalary()));
     }
 
     // DELETE - Delete Employee
     @DeleteMapping("/delete/{id}")
-    public String deleteEmployee(@PathVariable Long id) {
+    public boolean deleteEmployee(@PathVariable Long id) {
         return employeeService.deleteEmployee(id);
     }
 }
