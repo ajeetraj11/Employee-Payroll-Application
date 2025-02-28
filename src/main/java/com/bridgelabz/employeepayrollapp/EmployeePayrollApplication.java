1f03dff0ae8b113cb1ae30ac7874e70a65552dae
ajeetraj11 <ajitpandey110920@gmail.com>
2025-02-28 15:16:01 +0530

    [Ajeet Raj] Add. Lombok Logger for Logging the Information

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollApplication.java
@@ -1,13 +1,20 @@
 package com.bridgelabz.employeepayrollapp;
 
+import com.bridgelabz.employeepayrollapp.service.EmployeeService;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class EmployeePayrollApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(EmployeePayrollApplication.class, args);
+
+
 	}
 
 }
